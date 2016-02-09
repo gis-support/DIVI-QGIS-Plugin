7b982070f9e064b1b5b83847f8af866d050af164
Piotr Pociask <piotr.pociask@gis-support.pl>
2016-02-09 11:22:04 +0100

    initial commit

---
+++ __init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ DiviPlugin
+                                 A QGIS plugin
+ Integracja QGIS z platformą DIVI firmy GIS Support sp. z o. o.
+                             -------------------
+        begin                : 2016-02-09
+        copyright            : (C) 2016 by GIS Support sp. z o. o.
+        email                : info@gis-support.pl
+        git sha              : $Format:%H$
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load DiviPlugin class from file DiviPlugin.
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgsInterface
+    """
+    #
+    from .divi_plugin import DiviPlugin
+    return DiviPlugin(iface)
